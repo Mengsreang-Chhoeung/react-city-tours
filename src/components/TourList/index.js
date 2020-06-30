db8ac94bea6def5cb552ff18e1380823a97c30b2
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2020-06-30 10:58:52 +0700

    first commit

---
+++ src/components/TourList/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./TourList.scss";
+import Tour from "../Tour/Tour";
+import { tourData } from "../tourData";
+
+export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const sorted = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: sorted,
+    });
+  };
+  render() {
+    const { tours } = this.state;
+    return (
+      <section className="tourlist">
+        {tours.map((tour) => {
+          return (
+            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+          );
+        })}
+      </section>
+    );
+  }
+}
